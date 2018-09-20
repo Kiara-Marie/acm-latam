9c6c6d9aea47ae5a814ebceae1d5ee2691f1b4ae
kiarag <kiara.m.grant@gmail.com>
2018-09-20 11:17:52 -0700

    Better comments

--- C.cpp
+++ C.cpp
@@ -4,26 +4,28 @@
 using namespace std;
 
 int main(){
-	
+	// all numbers in the given sequence should appear an equal number of times
+	// but they do not. Remove one, add one, or remove one and add one,
+	// in order to make all numbers appear an equal number of times
+
+	// N = number of numbers
+	// K = range of numbers (numbers will range from 1-K)
 	int N,K;
 	cin >>K>>N;
 	int occupancy[K+1];
+
+// initialize matrix to 0
 	for (int i = 0; i<= K; i++){
 		occupancy[i] = 0;
 	}
-	for (int i = 1; i<= K; i++){
-		//cout<<i<<'-'<<occupancy[i]<<'\n';
-	}
+		// determine how many of each type of number there are
 	for (int i = 0; i< N; i++){
 		int currNum;
  		cin>>currNum;
 		occupancy[currNum]++;
 	}
-	//cout<<"See occupancies\n";
 
-	for (int i = 1; i<= K; i++){
-		//cout<<i<<'-'<<occupancy[i]<<'\n';
-	}
+// determine the expected number of times each number should appear
 	int expected = N%K<2? N/K:N/K +1;
 	int found = 0;
 	int foundMore = 0;
@@ -33,20 +35,17 @@ int main(){
 		for (int i = 1; i<=K; i++){
 			if(occupancy[i]!=expected){
 					if(found||occupancy[i]!=expected +1){
-						//cout<<__LINE__<<'\n';
 						cout<<'*'<<'\n';
 						return 0;
 					}
 					found = i;
-				} 
+				}
 		}
 		if (found){
 			cout<<'-'<<found<<'\n';
-						//cout<<__LINE__<<'\n';
 		}
 		else{
 		cout<<'*'<<'\n';
-						//cout<<__LINE__<<'\n';
 		}
 	}
 	else if (N%K == K - 1){
@@ -55,20 +54,16 @@ int main(){
 			if(occupancy[i]!=expected){
 					if(found||occupancy[i]!=expected -1){
 						cout<<'*'<<'\n';
-						//cout<<__LINE__<<'\n';
-						//cout<<i<<'\n';
 						return 0;
 					}
 					found = i;
-				} 
+				}
 		}
 		if (found){
 			cout<<'+'<<found<<'\n';
-						//cout<<__LINE__<<'\n';
 		}
 		else{
 		cout<<'*'<<'\n';
-						//cout<<__LINE__<<'\n';
 		}
 	}
 
@@ -79,32 +74,28 @@ int main(){
 					if(occupancy[i] == expected+1){
 						if(foundMore){
 							cout<<'*'<<'\n';
-						//cout<<__LINE__<<'\n';
 							return 0;
 							}
 						foundMore = i;
 					}
-					else if (occupancy[i] == expected -1){	
+					else if (occupancy[i] == expected -1){
 						if(foundLess){
 							cout<<'*'<<'\n';
-						//cout<<__LINE__<<'\n';
 							return 0;
 							}
 						foundLess = i;
 					}
 					else{
 						cout<<'*'<<'\n';
-						//cout<<__LINE__<<'\n';
 						return 0;
 					}
-				} 
+				}
 		}
 		if (foundMore && foundLess){
 			cout<<'-'<<foundMore<<' '<<'+'<<foundLess<<'\n';
 		}
 		else{
 		cout<<'*'<<'\n';
-						//cout<<__LINE__<<'\n';
 		}
 	}
 	else{
@@ -113,4 +104,4 @@ int main(){
 
 	}
 	return 0;
-} 
+}
